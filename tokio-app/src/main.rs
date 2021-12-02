a38b934a68fa7408d545f9e318d268d00d185d5f
Yota Toyama <raviqqe@gmail.com>
2021-12-02 02:45:09 +0000

    Use tokio functions

--- tokio-app/src/main.rs
+++ tokio-app/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::exit;
+use tokio::io::{stdout, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+    stdout().write("Hello, world!".as_bytes()).await.unwrap();
+
+    exit(0);
 }
