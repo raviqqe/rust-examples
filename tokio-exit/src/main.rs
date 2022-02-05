69778695220afa986771e4bc6808775628e88e6a
Yota Toyama <raviqqe@gmail.com>
2022-02-05 22:24:12 +0000

    Update exit example

--- tokio-exit/src/main.rs
+++ tokio-exit/src/main.rs
@@ -3,7 +3,7 @@ use tokio::io::{stdout, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
-    stdout().write("Hello, world!".as_bytes()).await.unwrap();
+    stdout().write("Hello, world!\n".as_bytes()).await.unwrap();
 
     exit(0);
 }
